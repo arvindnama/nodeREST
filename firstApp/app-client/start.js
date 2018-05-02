3f6d965f15fbde209562087c89ee5b37218f8a78
namaumaa <arvind.nama@roche.com>
2018-05-01 17:54:12 -0700

    Typescript adoption

--- firstApp/app-client/start.js
+++ firstApp/app-client/start.js
@@ -3,4 +3,4 @@ require('babel-register')({
 });
 
 // Import the rest of our application.
-module.exports = require('./server/server.js');
+module.exports = require('./client.js');
---
+++ firstApp/app-server/src/@types/json-server.d.ts
@@ -0,0 +1,7 @@
+declare namespace jsonServer {
+  //bodyParser: any;
+  function create(): any;
+  function defaults(): any;
+  function router(): any;
+  function rewriter(): any;
+}
